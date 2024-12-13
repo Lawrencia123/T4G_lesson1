632d5f6bbee6a3be386dca488c132875d7f9a9f2
Lawrencia Baaba Mensah <lawbabmensah@gmail.com>
2024-12-13 11:25:09 +0000

    made changes

---
+++ src/index.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+alert("Hello Lawrencia");
+prompt("What is your name");
+let sardine = prompt("How many sardines do you want to buy")
+alert(sardine + " it is!");
+alert("2 2");
+//alert 
+//variable naming convention
+//let friend;
+//let age;
+//let favcolor;
+
+let friend = prompt("what is your favourite friend's name")
+let age = prompt("what is her age")
+let favcolor= prompt("what is a her favorite color")
+alert(`My beautiful friends name is ${friend}. She is ${age} years old. Her favorite color is ${favcolor}`)
+
